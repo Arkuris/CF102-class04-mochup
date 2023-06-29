e25d10339985faf6ed114cc1642e3171444503d6
David Danilchik <davdd@mac.com>
2023-06-29 15:55:00 -0700

    did current 08 lab

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ function timebutton() {
   alert("Time for you to get a watch!")
 }
 
+
+function askUser(){
+    let number = prompt('On a Scale of 1-10, how much do you enjoy Architecture?');
+    while(!number){
+      number = prompt('Please tell me, I really want to know!')
+    }
+  for (let i = 0; i < number; i++) {
+    document.write(
+      '<img style="width:150px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Space_Needle_2011-07-04.jpg/800px-Space_Needle_2011-07-04.jpg">'
+    )
+  }   
+}
+
+
